7a8557f3bf09f7732aa4d4b63dd3dd79a213124c
arinzeJeffrey <nnamenearinze@gmail.com>
2020-08-31 15:13:34 +0100

    added the post route to add records to the database

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('country', "CountryController@country");
+Route::get('country/{id}', "CountryController@countryByID");
+Route::post('country', "CountryController@addCountry");
